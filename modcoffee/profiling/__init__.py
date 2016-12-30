466a3f38a5930e5a1d46a009166f32b9ae399d6b
Buck Baskin <buckbaskin@google.com>
2016-12-30 01:59:59 -0500

    Add doctests to profiling

--- modcoffee/profiling/__init__.py
+++ modcoffee/profiling/__init__.py
@@ -14,19 +14,46 @@ def cpu_total():
 def cpu_available():
     '''
     Equivalent to modcoffee.profiling.cpu_total
+
+    >>> isinstance(cpu_available(), int)
+    True
     '''
     # This is the same as total because of process switching. It might make
     #   sense to have this track the Docker cpu allocation
     return psutil.cpu_count()
 
 def mem_total():
+    '''
+    Returns the total (used + free) number of bytes available in memory
+
+    >>> isinstance(mem_total(), int)
+    True
+    '''
     return psutil.virtual_memory().total
 
 def mem_available():
+    '''
+    Returns the available (free) number of bytes available in memory
+
+    >>> isinstance(mem_available(), int)
+    True
+    '''
     return psutil.virtual_memory().available
 
 def disk_total():
+    '''
+    Returns the total (used + free) number of bytes available on disk
+
+    >>> isinstance(disk_total(), int)
+    True
+    '''
     return psutil.disk_usage('/').total
 
 def disk_available():
+    '''
+    Returns the available (free) number of bytes available in disk
+
+    >>> isinstance(disk_available(), int)
+    True
+    '''
     return psutil.disk_usage('/').free
