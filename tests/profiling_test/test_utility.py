363cb3673399e0a33c21bd8febcd0ea4f0b8ac89
Buck Baskin <buckbaskin@google.com>
2016-12-30 02:02:05 -0500

    Revise tests to dedup doctests

--- tests/profiling_test/test_utility.py
+++ tests/profiling_test/test_utility.py
@@ -1,19 +1,10 @@
 import modcoffee.profiling as mcp
 
-def test_cpu_total():
-    assert isinstance(mcp.cpu_total(), int)
+def test_cpu():
+    mcp.cpu_total() >= mcp.cpu_available()
 
-def test_cpu_available():
-    assert isinstance(mcp.cpu_available(), int)
+def test_mem():
+    mcp.mem_total() >= mcp.mem_available()
 
-def test_mem_total():
-    assert isinstance(mcp.mem_total(), int)
-
-def test_mem_available():
-    assert isinstance(mcp.mem_available(), int)
-
-def test_disk_total():
-    assert isinstance(mcp.disk_total(), int)
-
-def test_disk_available():
-    assert isinstance(mcp.disk_available(), int)
+def test_disk():
+    mcp.disk_total() >= mcp.disk_available()
